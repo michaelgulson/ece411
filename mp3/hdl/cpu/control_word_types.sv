0334567aea19a7bef998b22b7666e6f6d5ac2294
mgulson2 <mgulson2@illinois.edu>
2020-03-25 17:27:28 -0500

    added to datapath

---
+++ mp3/hdl/cpu/control_word_types.sv
@@ -0,0 +1,15 @@
+import rv32i_types::*;
+
+
+typedef struct packed {
+    alu_ops alu_op;
+    logic mem_read;
+    logic mem_write;
+    logic regfile_mux_sel;
+    logic [3:0] load_regfile;
+    logic pc_mux_sel;
+    logic alu_muxsel1;
+    logic [2:0] alu_muxsel2;
+    logic [4:0] dest;
+    
+} control_word;
--- mp3/hdl/cpu/datapath.sv
+++ mp3/hdl/cpu/datapath.sv
@@ -1,3 +1,6 @@
+import rv32i_types::*;
+
+
 module datapath
 {
     input clk,
@@ -15,7 +18,7 @@ module datapath
     output d_mem_read,
     output d_mem_write,
 
-}
+};
 
 
 /********************************Control Unit********************************/
@@ -28,6 +31,134 @@ module datapath
 
 
 /********************************Registers***********************************/
+//Other registers
+//pcreg
+
+//5 stage registers
+//IF/ID
+register pc_IF_ID(
+
+//I'll do this later
+
+);
+
+register ir_IF_ID(
+
+//I'll do this later
+
+); 
+
+//ID/EX
+
+register control_word_ID_EX(
+
+//I'll do this later
+
+); 
+
+register pc_IF_ID(
+
+//I'll do this later
+
+);
+
+register read_data1_ID_EX(
+
+//I'll do this later
+
+);
+
+register read_data2_ID_EX(
+    
+//I'll do this later
+
+);
+
+register imm_ID_EX(
+    
+//I'll do this later
+
+);
+
+//EX/MEM
+register control_word_EX_MEM(
+
+//I'll do this later
+
+); 
+
+register pc_EX_MEM(
+
+//I'll do this later
+
+);
+
+register br_en_EX_MEM(
+
+//I'll do this later
+
+);
+
+register pc_offset_EX_MEM(
+
+//I'll do this later
+
+);
+
+register read_data2_EX_MEM(
+
+//I'll do this later
+
+);
+
+register imm_EX_MEM(
+
+//I'll do this later
+
+);
+
+register ALUout_EX_MEM(
+
+//I'll do this later
+
+);
+
+//MEM/WB
+register control_word_MEM_WB(
+
+//I'll do this later
+
+); 
+
+register br_en_MEM_WB(
+
+//I'll do this later
+
+); 
+
+register pc_offset_MEM_WB(
+
+//I'll do this later
+
+);
+
+register data_out_MEM_WB(
+
+//I'll do this later
+
+);
+
+register alu_out_MEM_WB(
+
+//I'll do this later
+
+);
+
+register imm_MEM_WB(
+
+//I'll do this later
+
+);
 
 
 /****************************************************************************/
