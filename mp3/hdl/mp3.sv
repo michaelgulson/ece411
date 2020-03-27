3ce070324a929db94e12a0daf85c5cf9883a638a
mgulson2 <mgulson2@illinois.edu>
2020-03-27 13:59:33 -0500

    started mp3.sv

--- mp3/hdl/mp3.sv
+++ mp3/hdl/mp3.sv
@@ -3,11 +3,24 @@ import rv32i_types::*;
 
 module mp3
 (
-    input rst,
-    input pmem_resp,
-    input [63:0] pmem_rdata,
-    output logic pmem_read,
-    output logic pmem_write,
-    output rv32i_word pmem_address,
-    output [63:0] pmem_wdata
+    input logic clk,
+    input logic rst,
+
+    /* I Cache Ports */
+    output logic inst_read,
+    output logic [31:0] inst_addr,
+    input logic inst_resp,
+    input [31:0] inst_rdata;
+
+    /* D Cache Ports */
+    output logic data_read;
+    output logic data_write;
+    output logic [3:0] data_mbe;
+    output logic [31:0] data_addr;
+    output logic [31:0] data_wdata;
+    input logic data_resp;
+    input logic [31:0] data_rdata;
 );
+
+
+datapath pipeline_datapath(.*);
