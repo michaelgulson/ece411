38d0d9c8e94f852beb92d3e976406359f5988767
mgulson2 <mgulson2@illinois.edu>
2020-04-05 12:24:08 -0500

    changes to arbiter

--- mp3/hdl/arbiter/arbiter_datapath.sv
+++ mp3/hdl/arbiter/arbiter_datapath.sv
@@ -2,10 +2,13 @@ module arbiter_datapath(
     input logic mux_sel;
     input logic [31:0] mem_addr_i;
     input logic [31:0] mem_addr_d;
+    input logic [255:0] wdata_i;
+    input logic [255:0] wdata_d;
     input logic [255:0] rdata;
     output logic [31:0] mem_addr;
     output logic [255:0] inst_rdata;
     output logic [255:0] data_rdata;
+    output logic [255:0] wdata;
 );
 
 always_comb
@@ -15,11 +18,13 @@ begin
         begin
             mem_addr = mem_addr_i;
             inst_rdata = rdata;
+            wdata = wdata_i;
         end
         1'd1:
         begin
             mem_addr = mem_addr_d;
             data_rdata = rdata;
+            wdata = wdata_d;
         end
         default: 
             mem_addr = mem_addr_i; 
