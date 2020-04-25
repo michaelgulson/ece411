5f0350b4c9e273806ed3f183ddcbbb50b3dee3f3
nathans5 <nathans5@illinois.edu>
2020-04-24 22:56:56 -0500

    Revert "Revert "saving cache/arbiter""
    This reverts commit bf80d9d0df16dbd87fabb30d51aa945e6c87e64e.

--- mp3/hdl/arbiter/arbiter_control.sv
+++ mp3/hdl/arbiter/arbiter_control.sv
@@ -91,9 +91,13 @@ begin : next_state_logic
         end
         D_CACHE:
         begin
-            if(pmem_resp)
+            if(pmem_resp && (!mem_read_i))
             begin
                 next_state = IDLE;
+            end
+            else if(pmem_resp && (mem_read_i))
+            begin 
+                next_state = I_CACHE;
             end
             else
             begin
